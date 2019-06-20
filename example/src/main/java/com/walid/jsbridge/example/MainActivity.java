c1dcf882f84d16b3f6fbda008ee350d3c52a7568
walid <walid@soulapp.cn>
2019-06-20 19:50:18 +0800

    format code

--- example/src/main/java/com/walid/jsbridge/example/MainActivity.java
+++ example/src/main/java/com/walid/jsbridge/example/MainActivity.java
@@ -39,6 +39,22 @@ public class MainActivity extends Activity {
 
         BridgeModuleManager.registerModule(webView, TestModule.class);
 
+//        String scbs = "";
+//        ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+//        ClipData data = cm.getPrimaryClip();
+//        if (data != null) {
+//            int count = data.getItemCount();
+//            for (int i = 0; i < count; i++) {
+//                ClipData.Item item = data.getItemAt(i);
+//                if (item == null) continue;
+//                String text = (String) item.getText();
+//                if (!TextUtils.isEmpty(text) && text.startsWith("scbs=")) {
+//                    scbs = text.replaceFirst("scbs=", "");
+//                    break;
+//                }
+//            }
+//        }
+//        webView.loadUrl("http://172.29.90.163:62143/?scbs=" + scbs);
         webView.loadUrl("file:///android_asset/demo.html");
     }
 
